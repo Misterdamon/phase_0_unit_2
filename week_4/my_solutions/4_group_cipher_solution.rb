96e818ed79e6f7906590b822ed57f75092c96623
Jake Persing <misterdamon@gmail.com>
2014-04-06 12:05:19 -0700

    committed group challenge and challenge 5

--- week_4/my_solutions/4_group_cipher_solution.rb
+++ week_4/my_solutions/4_group_cipher_solution.rb
@@ -1,7 +1,7 @@
 # U2.W4: Cipher Challenge
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Sam Davis.
 
 
 
@@ -10,7 +10,7 @@
 # If you have difficulty, go into IRB and play with the methods.
 
 
-
+=begin
 def north_korean_cipher(coded_message)
   input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the ruby docs.
   decoded_sentence = []
@@ -68,12 +68,67 @@ def north_korean_cipher(coded_message)
   if decoded_sentence.match(/\d+/) #What is this matching? Look at Rubular for help. 
     decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } #He's been known to exaggerate...
   end  
-  return decoded_sentence # What is this returning?        
+  return decoded_sentence # What is this returning?  # What is this returning? It would be returning a string, due to the .join.           
 end
+=end
+
+#Samuel Davis
+#Jake Persing
+
 
 # Your Refactored Solution
-
-
+# defined an array "letters" from a to z
+def north_korean_cipher(coded_message)
+  input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the ruby docs.
+  decoded_sentence = []
+  letters = ("a".."z").to_a
+  # defined a new array "rotated" that is a rotated version of letters
+  rotated = letters.rotate(4)
+  # declared an empty hash "cipher"
+  cipher = {}
+  # iterated through the array by the index and reassigned each hash key
+  # to its corresponding shifted letter
+  rotated.each_with_index {|v, i| cipher[v] = letters[i]}
+  # If we changed it to 10 rotated letters, then it would be shifted 6 more letters to the right
+  # rotated = letter.rotate(10)
+  # the rest of the code would be the same
+ input.each do |x| # What is #each doing here? Each is iterating through each value 
+  # in the input so that it can compare it to each value in the cipher hash. 
+    found_match = false  # Why would this be assigned to false from the outset? What happens when it's true? Serves as a check so we know if we found
+    # any matches in the if and elsif checks. What happens when it's true? We found a match!
+    cipher.each_key do |y| # What is #each_key doing here? # Iterating through each key in the hash "cipher".
+      if x == y  # What is this comparing? It is comparing the input to the key in the cipher.
+      #Where is it getting x? It gets x from input.each, x is the corresponding character 
+      #in input. Where is it getting y? y is from cipher.each_key, and represents the key element from the cipher hash. What are those variables really? Those variables are placeholders for characters to compare.
+        puts "I am comparing x and y. X is #{x} and Y is #{y}."
+        decoded_sentence << cipher[y]
+        found_match = true
+        break  # Why is it breaking here?
+        # it breaks so that the found_match flag can be evaluated and so that the other conditional branches are not compared
+      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #What the heck is this doing? 
+        #This is comparing the input value to a variety of symbols, if x contains one of these symbols, push a space character into the decoded sentence.
+        decoded_sentence << " "
+        found_match = true
+        break
+      elsif (0..9).to_a.include?(x) # Try this out in IRB. What does   " (0..9).to_a "    do?
+        #This creates an array containing 10 numbers from 0-9
+        decoded_sentence << x
+        found_match = true
+        break
+      end 
+    end
+    if not found_match  # What is this looking for? This is looking checking to see if the found_match flag is false, if it is, then we push x into the decoded sentence.
+      decoded_sentence << x
+    end
+  end
+  decoded_sentence = decoded_sentence.join("")
+ 
+  if decoded_sentence.match(/\d+/) #What is this matching? Look at Rubular for help. 
+    # this is a regular expression that looks for a match of an number of any length, and divides by 100 
+    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } #He's been known to exaggerate...
+  end  
+  return decoded_sentence # What is this returning? It would be returning a string, due to the .join.           
+end
 
 
 
@@ -87,4 +142,10 @@ p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!")
 p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?")
 
 # Reflection
- 
+=begin
+  In this group challenge, it took us a while to figure out how to refactor the cipher hash to be a simple algorithm, but we eventually got it right. Our strategy was to go through the code, 
+  and pick out which areas could use improvement. I guess the only real question I had while working through this challenge was on the purpose of the found_match flag, and why it is necessary 
+  in this case. I still get confused by the enumerable methods sometimes, especially the block portions, there seem to be so many methods that differ only slightly, so it is difficult to understand how they each
+  work without researching the documentation for them. Overall I still feel comfortable with the learning objectives so far, but could definitely use as much practice as I can get. I found refactoring somehwat 
+  tedious just because it is difficult for me to think of better code that can improve on the original without completely changing the program architecture. 
+=end
--- week_4/my_solutions/5_reverse_cipher_solution.rb
+++ week_4/my_solutions/5_reverse_cipher_solution.rb
@@ -1,7 +1,7 @@
 # U2.W4: Refactor Cipher Solution
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 
 # 1. Solution
@@ -11,39 +11,68 @@
 # Try implementing the code without it. 
 
 def translate_to_cipher(sentence)
-    alphabet = ('a'..'z').to_a
-    cipher = Hash[alphabet.zip(alphabet.rotate(4))]
-    spaces = ["@", "#", "$", "%", "^", "&", "*"]
+    alphabet = ('a'..'z').to_a #declares alphabet to be a range of characters from a-z, which is then converted into an array
+    cipher = Hash[alphabet.zip(alphabet.rotate(4))] #rotate() shifts the values of the alphabets by 4 characters, and zip assigns each letter in the alphabet to a Hash key which contains the shifted alphabet letter as a value. i.e. a => e, g => k, etc...
+    spaces = ["@", "#", "$", "%", "^", "&", "*"] #hardcodes characters that are correspond to spaces into an array
     
-    original_sentence = sentence.downcase
-    encoded_sentence = []
-    original_sentence.each_char do |element|
-      if cipher.include?(element)
-        encoded_sentence << cipher[element]
-      elsif element == ' '
-        encoded_sentence << spaces.sample
-      else 
-        encoded_sentence << element
+    original_sentence = sentence.downcase # converts original sentence to lower case
+    encoded_sentence = [] #declares encoded sentence as an empty array
+    original_sentence.each_char do |element| #iterates through each character from the original sentence
+      if cipher.include?(element) #if the cipher Hash contains a key that corresponds to the original sentence element we are looking at--
+        encoded_sentence << cipher[element] #push the corresponding coded value into the coded sentence array
+      elsif element == ' ' #if the character in original sentence is a space--
+        encoded_sentence << spaces.sample #insert a random symbol from the spaces array, i.e. one of @ # $ % ^ & or *
+      else #if the cipher Hash doesn't contain the element and the character is not a space, then--
+        encoded_sentence << element #push the element into the encoded sentence array, such as punctuation marks
       end
      end
     
-    return encoded_sentence.join
+    return encoded_sentence.join #joins the array of encoded characters into a string and returns it
 end
 
 
 # Questions:
 # 1. What is the .to_a method doing?
+=begin
+  The .to_a method is converting a range of characters from a-z into an array. 
+  The result is an array containing each letter of the alphabet
+=end
 # 2. How does the rotate method work? What does it work on?
+=begin 
+  The rotate method works by taking whatever array that it is called on and shifts its indices by the specified value.
+  In this case, rotate shifts the letters of the alphabet by +4 which shifts each letter to the left by 4, and wraps those at the beginning of the array back to the end.
+  So the calling alphabet.rotate(4) would produce %w[e f g h i j k l m n o p q r s t u v w x y z a b c d].
+=end
 # 3. What is `each_char` doing?
+=begin
+  The each_char method takes each character in the original sentence and passes it to the given block. 
+  In this case the block is an if/else branch that determines how to encode each character.
+=end
 # 4. What does `sample` do?
+=begin
+  sample takes a random value from the array it is called on, in this case, sample is taking a random symbol @ # $ % ^ &  or * and inserting it 
+  into the coded sentence in the place of a space. So each time there is a space, a random symbol is substituted for it.
+=end
 # 5. Are there any other methods you want to understand better?
+=begin
+  I would like to get to know the zip method a little better, while I understand that in this example it maps each Hash key to the corresponding hash value, I don't fully
+  understand how it works at this point. 
+=end 
 # 6. Does this code look better or worse than your refactored solution
 #    of the original cipher code? What's better? What's worse?
+=begin
+  I think this code looks way better than my refactored solution, particularly the method used to assign the hash key/values, my method of accomplishing that ended up being longer and less efficient.
+  Also, the conditional branching was better accomplished in this solution than my own.
+=end
+
 # 7. Is this good code? What makes it good? What makes it bad?
-
-
+=begin
+  I feel that this is good code, because it is clear, concise, and uses many ruby methods that I didn't know existed yet. I liked how the alphabet/hash/space values are not all hard-coded, and this makes it easier to follow.
+  I also think this code is rather self-explanatory, except for the zip method, which I found a bit hard to understand in relation to assigning the Hash keys/values
+=end
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # Does this return the same thing every time?
+#No this code does not return the same thing every time, while the encoded alphabetical characters are the same, each space is represented by a random sample of a symbol from the spaces array, so it produces a different output each time. 
 p translate_to_cipher("I want cookies")
 p translate_to_cipher("I want cookies")
 p translate_to_cipher("I want cookies")
@@ -54,4 +83,11 @@ p translate_to_cipher("I want cookies")
 
 
 # 5. Reflection 
-
+=begin
+  For this challenge, I understood what the majority of the code was doing and was able to explain it. It took me a while to understand how the cipher Hash was properly assigning
+  its key/value pairs from the zip and rotate methods, but after playing around in irb with it for a while I began to comprehend it. I guess the only trouble I'm having is trying to figure out why
+  the code that I write is so different from the code other people write. I constantly find myself implementing these really complicated methods only to find that ruby already has one specific to what 
+  I'm trying to do, I guess it just takes practice and a deeper understanding of the full scope of ruby's built-in methods. I learned a lot in this challenge though, including a few new methods that I hadn't
+  tried before, like sample, zip and rotate, and it was great to get some exposure to them. So far I'm pretty confident in the learning objectives and can't wait to start the week 5 challenges! I enjoyed this exercise greatly
+  because I got to see someone else's code and got some new insight into how mine should ultimately function.
+=end
