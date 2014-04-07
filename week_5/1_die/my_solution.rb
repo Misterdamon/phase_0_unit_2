b73ec3a43938331416f65056621e52db6a4b77d5
Jake Persing <misterdamon@gmail.com>
2014-04-07 16:43:17 -0700

    committed challenges 1 and 2 for week 5

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,41 +1,77 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
 # Input:
+	#the input in this case would be a number of sides for the die, the input is only needed when initializing a dice
 # Output:
+	# depending on the class function called on the die, the ouput will differ. The output of Die.sides would be the number of sides given in the initialization.
+	# the output of the Die.roll function will be a random number between 1 and the number of sides
 # Steps:
-
-
+=begin
+	Pseudocode:
+		-- define class Die
+			-- define function initialize() to take a single integer as a parameter.
+			 -- if sides < 1, raise Argument error
+			 -- set instance var sides to sides argument of initialize
+			-- define function sides()
+				- return @sides
+			-- define function roll()
+				- define a range from 1 to the number of sides
+				 -- convert this range to an array
+				 -- call sample on this array and return that number
+=end
 # 3. Initial Solution
-
+=begin
 class Die
   def initialize(sides)
-    # code goes here
+  	if sides < 1
+  	raise ArgumentError.new("Dice must have a positive number of sides.")
+  	end
+    @sides = sides
   end
   
   def sides
-    # code goes here
+    @sides
   end
   
   def roll
-    # code goes here
+   (1..@sides).to_a.sample
+  end
+end
+
+=end
+
+# 4. Refactored Solution
+class Die
+  def initialize(sides)
+  	raise ArgumentError.new("Dice must have a positive number of sides.") if sides < 1
+    @sides = sides
+  end
+  
+  def sides
+    @sides
+  end
+  
+  def roll
+   rand(1..@sides)
   end
 end
 
 
-
-# 4. Refactored Solution
-
-
-
-
-
-
 # 1. DRIVER TESTS GO BELOW THIS LINE
+p Die.new(6).roll.is_a? Fixnum 
+p Die.new(100).sides == 100
+p Die.new(6).sides == 6
+begin
+	Die.new(0)
+rescue ArgumentError => a
+	fail = a.message
+end
+p fail == "Dice must have a positive number of sides."
 
 
 
@@ -43,3 +79,9 @@ end
 
 
 # 5. Reflection 
+=begin
+	For this challenge, my process was basically to look at the rspec tests to determine what requirements needed to be satisfied. This was simple enough to do
+	because I had worked with classes and rspec tests before. I learned how to use begin/rescue/end to catch an argument error, which was nice to know, because 
+	I can implement argument tests without using rspec. Overall, I feel comfortable with the learning objectives so far and am excited to learn more about classes
+	in the coming weeks. I enjoyed this challenge a lot because I got to use instance variables and learned how to write more complicated driver test code.
+=end
--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -1,42 +1,81 @@
 # U2.W5: Die Class 2: Arbitrary Symbols
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
 # Input:
+ #the input to initialize a die class would be an array of strings
 # Output:
+ #the output of Die.sides would be the length of the strings array
+ #the output of Die.roll will be a single random string from the strings array
 # Steps:
-
-
+=begin
+	Pseudocode:
+	-- define Die class
+		-- define initialize() to take a single array of strings
+		 -- raise argument error if labels array is empty
+		 -- set @labels to labels
+		-- define sides()
+			- return length of labels array
+		-- define roll
+			- return random value of labels array
+=end
 # 3. Initial Solution
-
+=begin
 class Die
   def initialize(labels)
+  	raise ArgumentError.new("Labels list empty.") if labels == []
+  	@labels = labels
   end
 
   def sides
+  	@labels.length
   end
 
   def roll
+  	@labels[rand(0..@labels.length - 1)]
+  end
+end
+=end
+# 4. Refactored Solution
+
+class Die
+  def initialize(labels)
+  	raise ArgumentError.new("Labels list empty.") if labels == []
+  	@labels = labels
+  end
+
+  def sides
+  	@labels.length
+  end
+
+  def roll
+  	@labels.sample
   end
 end
 
 
-
-# 4. Refactored Solution
-
-
-
-
-
-
 # 1. DRIVER TESTS GO BELOW THIS LINE
-
-
-
+begin 
+	Die.new([])
+rescue ArgumentError => a
+	fail = a.message
+end
+p fail == "Labels list empty."
+p Die.new(['a','b','c']).roll.is_a? String
+p Die.new(['a']).roll == 'a'
+p Die.new(['a','b','c']).sides == 3
+p Die.new(['a']).sides == 1
 
 
 
 # 5. Reflection 
+=begin
+	I found this challenge to be very enjoyable because I got some more practice with classes and class methods.
+	My strategy was essentially the same as the other dice challenge, where I would look at the rspec tests, translate them 
+	into driver test code, and then start writing pseudocode to follow. Since I followed these steps, writing the actual code was 
+	very simple. I liked how I got some more practice with test-driver development. Overall I feel comfortable with the learning objectives,
+	but could always use more practice. I enjoyed this challenge greatly because I got to use some more complicated methods.
+=end
